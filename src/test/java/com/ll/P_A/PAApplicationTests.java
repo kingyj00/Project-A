e2804bc6478dc39fffaac85ec50255bbb8829bbb
kingyj00 <onshopyj@gmail.com>
2025-04-14 10:53:33 +0900

    Setting

---
+++ src/test/java/com/ll/P_A/PAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.P_A;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
