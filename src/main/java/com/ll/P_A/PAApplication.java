c900ba92d85d8723dc9de5b4e1c2b603c24136d8
kingyj00 <onshopyj@gmail.com>
2025-04-24 00:21:26 +0900

    [TEST] 시작명력어 입력 시, 실행

--- src/main/java/com/ll/P_A/PAApplication.java
+++ src/main/java/com/ll/P_A/PAApplication.java
@@ -3,11 +3,19 @@ package com.ll.P_A;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class PAApplication {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("입력: ");
+		String input = scanner.nextLine();
+
+		System.out.println("입력값 : " + input);
+
 		SpringApplication.run(PAApplication.class, args);
 	}
-
 }
