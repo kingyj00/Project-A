f95ab246206312199150e829f08b2d9bbd339014
kingyj00 <onshopyj@gmail.com>
2025-05-29 00:10:45 +0900

    test

--- src/main/java/com/ll/P_A/PAApplication.java
+++ src/main/java/com/ll/P_A/PAApplication.java
@@ -2,11 +2,16 @@ package com.ll.P_A;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class PAApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(PAApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(PAApplication.class, args);
+		log.info("HELLO"); // 로그에 출력
 	}
 }
