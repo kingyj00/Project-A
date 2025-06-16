cdf4d581b485b55c9a55ff6e79f08537175eb4b3
kingyj00 <onshopyj@gmail.com>
2025-06-16 21:32:04 +0900

    [Feat] GlobalExceptionHandler

---
+++ src/main/java/com/ll/P_A/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.ll.P_A.global.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, String>> handleIllegalArgument(IllegalArgumentException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", ex.getMessage());
+        return ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handleOther(Exception ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "알 수 없는 오류가 발생했습니다.");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
+}
