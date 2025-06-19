79be2c0a7df7a99f62b3b5907a832aa82c2e4ed4
kingyj00 <onshopyj@gmail.com>
2025-06-19 22:44:52 +0900

    [Refactor] ResponseDTO for User

--- src/main/java/com/ll/P_A/post/PostResponseDto.java
+++ src/main/java/com/ll/P_A/post/PostResponseDto.java
@@ -1,12 +1,14 @@
 package com.ll.P_A.post;
 
+import com.ll.P_A.security.User;
+
 import java.time.LocalDateTime;
 
 public record PostResponseDto(
         Long id,
         String title,
         String content,
-        String author,
+        String authorName,     //변경: author → authorName
         int viewCount,
         int likeCount,
         LocalDateTime createdAt,
@@ -17,11 +19,16 @@ public record PostResponseDto(
                 post.getId(),
                 post.getTitle(),
                 post.getContent(),
-                post.getAuthor(),
+                extractAuthorName(post.getAuthor()),
                 post.getViewCount(),
                 post.getLikeCount(),
                 post.getCreatedAt(),
                 post.getUpdatedAt()
         );
+    }
+
+    private static String extractAuthorName(User author) {
+        if (author == null) return "(알 수 없음)";
+        return author.getUsername();  // 또는 getNickname(), getEmail() 등 원하는 필드
     }
 }
