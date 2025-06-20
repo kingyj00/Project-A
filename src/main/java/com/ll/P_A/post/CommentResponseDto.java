3a0e12978d2d48994f120d4fb101195d0385a8ec
kingyj00 <onshopyj@gmail.com>
2025-06-21 01:30:50 +0900

    [Feat] 댓글 기능 ResponseDTO

---
+++ src/main/java/com/ll/P_A/post/CommentResponseDto.java
@@ -0,0 +1,14 @@
+package com.ll.P_A.post;
+
+import java.time.LocalDateTime;
+
+public record CommentResponseDto(
+        Long id,
+        String content,
+        String authorName,
+        LocalDateTime createdAt
+) {
+    public CommentResponseDto(Comment comment) {
+        this(comment.getId(), comment.getContent(), comment.getAuthor().getUsername(), comment.getCreatedAt());
+    }
+}
