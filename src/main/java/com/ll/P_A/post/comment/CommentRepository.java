c105d7854ceb7f99c14ed2c692a857049616a265
kingyj00 <onshopyj@gmail.com>
2025-06-21 01:41:46 +0900

    [Feat] 댓글 기능 Repository

---
+++ src/main/java/com/ll/P_A/post/comment/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.ll.P_A.post.comment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPostIdOrderByCreatedAtAsc(Long postId);
+}
