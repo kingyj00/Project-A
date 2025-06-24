7fce3253e663a35694bab328cb71413dba04183f
kingyj00 <onshopyj@gmail.com>
2025-06-24 22:11:03 +0900

    [Feat] Post DTO 유효성 검사

--- src/main/java/com/ll/P_A/post/PostRequestDto.java
+++ src/main/java/com/ll/P_A/post/PostRequestDto.java
@@ -1,7 +1,14 @@
 package com.ll.P_A.post;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record PostRequestDto(
+
+        @NotBlank(message = "제목은 비어 있을 수 없습니다.")
         String title,
+
+        @NotBlank(message = "내용은 비어 있을 수 없습니다.")
         String content,
-        String author
-) {}
+
+        String author // 현재는 서버 측에서 유저 정보로 설정되므로 유효성 검사 대상 아님
+) { }
