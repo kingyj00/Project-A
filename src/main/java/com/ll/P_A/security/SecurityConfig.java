2fd4c8332d308d6a8f2cf9411b0355ec5dc0073b
kingyj00 <onshopyj@gmail.com>
2025-07-16 02:55:33 +0900

    [Fix] filterChain 중복 제거

--- src/main/java/com/ll/P_A/security/SecurityConfig.java
+++ src/main/java/com/ll/P_A/security/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
+    @Bean(name = "defaultSecurityFilterChain")
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
