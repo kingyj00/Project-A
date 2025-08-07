1348f31aaf4e03b2649ffb3b5012c139315b286f
kingyj00 <onshopyj@gmail.com>
2025-08-07 19:06:50 +0900

    [Fix] 게시글 NotBlack Message

--- src/main/java/com/ll/P_A/post/PostRequestDto.java
+++ src/main/java/com/ll/P_A/post/PostRequestDto.java
@@ -4,10 +4,10 @@ import jakarta.validation.constraints.NotBlank;
 
 public record PostRequestDto(
 
-        @NotBlank(message = "제목은 비어 있을 수 없습니다.")
+        @NotBlank(message = "제목을 입력해주세요.")
         String title,
 
-        @NotBlank(message = "내용은 비어 있을 수 없습니다.")
+        @NotBlank(message = "내용을 입력해주세요.")
         String content,
 
         String author // 현재는 서버 측에서 유저 정보로 설정되므로 유효성 검사 대상 아님
