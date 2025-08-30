5711249ae84e9f98edaf05c913d9e0e501e46a27
kingyj00 <onshopyj@gmail.com>
2025-08-31 00:21:58 +0900

    [Feat] 게시글 페이징/검색 추가-repo

--- src/main/java/com/ll/P_A/post/PostRepository.java
+++ src/main/java/com/ll/P_A/post/PostRepository.java
@@ -1,8 +1,17 @@
 package com.ll.P_A.post;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
+
+    //제목이나 내용에 키워드가 포함된 게시글 검색 (대소문자 무시, 페이징 지원)
+    Page<PostEntity> findByTitleContainingIgnoreCaseOrContentContainingIgnoreCase(
+            String title,
+            String content,
+            Pageable pageable
+    );
 }
