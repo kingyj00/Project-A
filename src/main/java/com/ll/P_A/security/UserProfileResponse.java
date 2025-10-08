a65da0c5ac69793a0fc8044e167655bb93b21475
kingyj00 <onshopyj@gmail.com>
2025-10-08 23:52:42 +0900

    [Feat] 스웨거용 스키마, Bean Validation 추가

--- src/main/java/com/ll/P_A/security/UserProfileResponse.java
+++ src/main/java/com/ll/P_A/security/UserProfileResponse.java
@@ -1,16 +1,19 @@
 package com.ll.P_A.security;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class UserProfileResponse {
-    private final String username;
-    private final String email;
-    private final boolean emailVerified;
 
-    public UserProfileResponse(String username, String email, boolean emailVerified) {
-        this.username = username;
-        this.email = email;
-        this.emailVerified = emailVerified;
-    }
+    @Schema(description = "아이디", example = "june11")
+    private final String username;
+
+    @Schema(description = "이메일", example = "june11@example.com")
+    private final String email;
+
+    @Schema(description = "이메일 인증 여부", example = "true")
+    private final boolean emailVerified;
 }
