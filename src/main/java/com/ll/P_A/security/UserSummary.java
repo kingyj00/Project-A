c4ca82b7bcdcb282846fea0d859531c3d8b30438
kingyj00 <onshopyj@gmail.com>
2025-10-08 23:52:44 +0900

    [Feat] 스웨거용 스키마, Bean Validation 추가

--- src/main/java/com/ll/P_A/security/UserSummary.java
+++ src/main/java/com/ll/P_A/security/UserSummary.java
@@ -1,7 +1,22 @@
 package com.ll.P_A.security;
 
-public interface UserSummary {
-    Long getId();
-    String getUsername();
-    String getEmail();
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserSummary {
+
+    @Schema(description = "사용자 ID", example = "1")
+    private final Long id;
+
+    @Schema(description = "아이디", example = "june11")
+    private final String username;
+
+    @Schema(description = "이메일", example = "june11@example.com")
+    private final String email;
+
+    @Schema(description = "닉네임", example = "준이")
+    private final String nickname;
 }
