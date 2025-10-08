cdeab8be621528569abdd6d6215314b2a2f66087
kingyj00 <onshopyj@gmail.com>
2025-10-08 23:51:16 +0900

    [Feat] 스웨거용 스키마, Bean Validation 추가

--- src/main/java/com/ll/P_A/security/LoginRequest.java
+++ src/main/java/com/ll/P_A/security/LoginRequest.java
@@ -1,11 +1,21 @@
 package com.ll.P_A.security;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Getter
-@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
+
+    @Schema(description = "로그인 아이디", example = "june11")
+    @NotBlank
     private String username;
+
+    @Schema(description = "로그인 비밀번호", example = "P@ssw0rd!23")
+    @NotBlank
     private String password;
 }
