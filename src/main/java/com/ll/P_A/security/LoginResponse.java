4fa88a208e20c2bd7c1c4038652f52c1edde8808
kingyj00 <onshopyj@gmail.com>
2025-10-08 23:52:40 +0900

    [Feat] 스웨거용 스키마, Bean Validation 추가

--- src/main/java/com/ll/P_A/security/LoginResponse.java
+++ src/main/java/com/ll/P_A/security/LoginResponse.java
@@ -1,6 +1,16 @@
 package com.ll.P_A.security;
 
-public record LoginResponse(
-        String accessToken,
-        String refreshToken
-) {}
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginResponse {
+
+    @Schema(description = "Access 토큰(JWT)", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6...")
+    private final String accessToken;
+
+    @Schema(description = "Refresh 토큰(JWT)", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6...")
+    private final String refreshToken;
+}
