6b90f43e3acc73a8ce2e5fd0e0b4c53ac731019c
kingyj00 <onshopyj@gmail.com>
2025-10-09 12:00:00 +0900

    [Feat] 스웨거용 스키마, Bean Validation 추가

--- src/main/java/com/ll/P_A/security/jwt/RefreshTokenRequest.java
+++ src/main/java/com/ll/P_A/security/jwt/RefreshTokenRequest.java
@@ -1,3 +1,10 @@
 package com.ll.P_A.security.jwt;
 
-public record RefreshTokenRequest(String refreshToken) {}
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+
+public record RefreshTokenRequest(
+        @Schema(description = "Refresh 토큰(JWT)", example = "eyJhbGciOiJIUzI1NiIs...")
+        @NotBlank
+        String refreshToken
+) {}
