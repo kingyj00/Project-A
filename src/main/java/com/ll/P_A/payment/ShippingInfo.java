64055f06168314abfc08871cfa6232f6453f0c31
kingyj00 <onshopyj@gmail.com>
2025-10-13 20:25:11 +0900

    [Feat] 결제- ShippingInfo

---
+++ src/main/java/com/ll/P_A/payment/ShippingInfo.java
@@ -0,0 +1,17 @@
+package com.ll.P_A.payment;
+
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Embeddable
+public class ShippingInfo {
+    private String receiverName;
+    private String phone;
+    private String address1;
+    private String address2;
+    private String zipcode;
+}
