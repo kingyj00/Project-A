6d894fd9d67c84817760918d662367454c29a8cc
kingyj00 <onshopyj@gmail.com>
2025-11-18 23:41:54 +0900

    [Feat] 주문자 확인 결제 메서드 추가

--- src/main/java/com/ll/P_A/payment/payment/PaymentRepository.java
+++ src/main/java/com/ll/P_A/payment/payment/PaymentRepository.java
@@ -5,6 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    // 주문 ID로 결제 조회
+    Optional<Payment> findByOrderId(Long orderId);
+
+    // paymentKey(Toss transaction key)로 조회
+    Optional<Payment> findByPaymentKey(String paymentKey);
+
+    // 기반 중복 결제 차단용
     Optional<Payment> findByIdempotencyKey(String idempotencyKey);
-    Optional<Payment> findByTransactionId(String transactionId);
 }
